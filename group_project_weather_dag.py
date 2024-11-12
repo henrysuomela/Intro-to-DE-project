3a671c721a9acafb035088a13721e8bb453f1781
Henry Suomela <144253010+henrysuomela@users.noreply.github.com>
2024-11-12 15:30:10 +0200

    group_project_weather_dag.py

--- group_project_weather_dag.py
+++ group_project_weather_dag.py
@@ -1,1 +1,17 @@
 import pandas as pd
+import sqlite3
+import os
+import zipfile
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+# Values can be changed
+default_args = {
+    'owner': 'Team_6',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 11, 6),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}

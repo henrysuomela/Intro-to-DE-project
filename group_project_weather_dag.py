dd72554ae24a6bdb5e71ac5a53fc8775527e9e76
Henry Suomela <144253010+henrysuomela@users.noreply.github.com>
2024-11-17 18:33:09 +0200

    transform_data now contains data cleaning and feature engineering, everything should work

--- group_project_weather_dag.py
+++ group_project_weather_dag.py
@@ -29,38 +29,8 @@ dag = DAG(
 
 
 
-def determine_wind_strength(wind_speed):
-    # Converts km/h to m/s and categorizes the wind strength 
-    wind_speed_m_per_s = wind_speed / 3.6
-
-    if wind_speed_m_per_s <= 1.5:
-        return 'Calm'
-    elif 1.6 <= wind_speed_m_per_s <= 3.3:
-        return 'Light Air'
-    elif 3.4 <= wind_speed_m_per_s <= 5.4:
-        return 'Light Breeze'
-    elif 5.5 <= wind_speed_m_per_s <= 7.9:
-        return 'Gentle Breeze'
-    elif 8.0 <= wind_speed_m_per_s <= 10.7:
-        return 'Moderate Breeze'
-    elif 10.8 <= wind_speed_m_per_s <= 13.8:
-        return 'Fresh Breeze'
-    elif 13.9 <= wind_speed_m_per_s <= 17.1:
-        return 'Strong Breeze'
-    elif 17.2 <= wind_speed_m_per_s <= 20.7:
-        return 'Near Gale'
-    elif 20.8 <= wind_speed_m_per_s <= 24.4:
-        return 'Gale'
-    elif 24.5 <= wind_speed_m_per_s <= 28.4:
-        return 'Strong Gale'
-    elif 28.5 <= wind_speed_m_per_s <= 32.6:
-        return 'Storm'
-    elif wind_speed_m_per_s >= 32.7:
-        return 'Violent Storm'
-
-
 def transform_data(**kwargs):
-    csv = kwargs['ti'].xcom_pull(key = 'INSERT ACTUAL KEY HERE')
+    csv = kwargs['ti'].xcom_pull(key='INSERT ACTUAL KEY HERE')
     df = pd.read_csv(csv)
 
     # Convert formatted date to datetime object, converting to utc and remove timezone info
@@ -71,12 +41,10 @@ def transform_data(**kwargs):
     df.drop_duplicates(subset='Formatted Date', inplace=True)
 
     # Precip type has two different values, rain and snow
-    # Fill nan values with snow if temperature is 0 or below, rain otherwise
+    # Fill NaN values with snow if temperature is 0 or below, rain otherwise
     fill_values = np.where(df['Temperature (C)'] <= 0, 'snow', 'rain')
     df['Precip Type'] = df['Precip Type'].fillna(pd.Series(fill_values, index=df.index))
 
-    # Column 'Loud Cover' probably means cloud cover, and only contains the value 0.0, let's remove it
-    df = df.drop(columns=['Loud Cover'])
 
 
     """ Replacing possible negative values with '?' in columns where value can't be negative,
@@ -89,7 +57,7 @@ def transform_data(**kwargs):
     df[columns_to_check] = df[columns_to_check].where(df[columns_to_check] >= 0, '?')
 
     # Replace '?' with NaN in the whole dataframe
-    df.replace('?', np.nan, inplace=True)
+    df.replace('?', pd.NA, inplace=True)
 
     # Columns where NaN should be replaced with mode
     columns_for_mode = columns_to_check + ['Temperature (C)', 'Apparent Temperature (C)']
@@ -98,3 +66,64 @@ def transform_data(**kwargs):
     for column in columns_for_mode:
         mode_value = df[column].mode()[0]
         df[column] = df[column].fillna(mode_value)
+
+
+    # Create new column for date without time of day
+    df['Date'] = df['Formatted Date'].dt.date
+
+    # Create new dataframe for daily averages grouping by date
+    daily_averages_df = df.groupby('Date')[['Temperature (C)', 'Humidity', 'Wind Speed (km/h)']].agg('mean').reset_index()
+
+
+    """ Adding new columns for current month and monthly mode """
+
+    # Create new column for month
+    df['Month'] = df['Formatted Date'].dt.month
+
+    # Return mode if there's only one mode, NaN if there are multiple
+    def calculate_mode(precip):
+        modes = precip.mode()
+        if len(modes) == 1:
+            return modes.iloc[0]
+        else:
+            return pd.NA
+
+    # Determine mode for each month
+    monthly_mode = df.groupby('Month')['Precip Type'].apply(calculate_mode)
+
+    # Create Mode column to original dataframe
+    df['Mode'] = df['Month'].map(monthly_mode)
+
+
+    """ Adding new column for wind strength """
+
+    def determine_wind_strength(wind_speed):
+        # Converts km/h to m/s and categorizes the wind strength 
+        wind_speed_m_per_s = round(wind_speed / 3.6, 1)
+
+        if wind_speed_m_per_s <= 1.5:
+            return 'Calm'
+        elif 1.6 <= wind_speed_m_per_s <= 3.3:
+            return 'Light Air'
+        elif 3.4 <= wind_speed_m_per_s <= 5.4:
+            return 'Light Breeze'
+        elif 5.5 <= wind_speed_m_per_s <= 7.9:
+            return 'Gentle Breeze'
+        elif 8.0 <= wind_speed_m_per_s <= 10.7:
+            return 'Moderate Breeze'
+        elif 10.8 <= wind_speed_m_per_s <= 13.8:
+            return 'Fresh Breeze'
+        elif 13.9 <= wind_speed_m_per_s <= 17.1:
+            return 'Strong Breeze'
+        elif 17.2 <= wind_speed_m_per_s <= 20.7:
+            return 'Near Gale'
+        elif 20.8 <= wind_speed_m_per_s <= 24.4:
+            return 'Gale'
+        elif 24.5 <= wind_speed_m_per_s <= 28.4:
+            return 'Strong Gale'
+        elif 28.5 <= wind_speed_m_per_s <= 32.6:
+            return 'Storm'
+        elif wind_speed_m_per_s >= 32.7:
+            return 'Violent Storm'
+
+    df['wind_strength'] = df['Wind Speed (km/h)'].apply(determine_wind_strength)
